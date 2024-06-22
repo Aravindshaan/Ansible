f1b2350d5dda7831a22dc1af712c5387f220a0dc
Aravindshaan <90775234+Aravindshaan@users.noreply.github.com>
2024-06-22 22:16:18 +0530

    Updated vpc-subnet-sg-ec2.tf

--- vpc-subnet-sg-ec2.tf
+++ vpc-subnet-sg-ec2.tf
@@ -9,6 +9,7 @@ resource "aws_subnet" "subnet1" {
   vpc_id=aws_vpc.vpc_1.id
   cidr_block="10.0.0.0/24"
   availability_zone="ap-south-1b"
+  map_public_ip_on_launch = true
 }
 resource "aws_security_group" "allow_ssh" {
   name="allow ssh"
